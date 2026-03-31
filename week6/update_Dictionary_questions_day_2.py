25095c9362bb21ecd7a468fedb118ececd3d115c
Andrew Murphy <legionastartesofficial@gmail.com>
2026-04-01 01:41:45 +0900

    Add files via upload

---
+++ week6/update_Dictionary_questions_day_2.py
@@ -0,0 +1,49 @@
+#practice questions for updating dictionary
+
+#question 1
+car = {
+    "brand": "Toyota",
+    "model": "Supra",
+    "year": 2020
+}
+
+
+car["year"] = 2024
+
+#how to add a key
+car["color"] = "red"
+
+print(car)
+
+
+#question 2
+user = {
+    "username": "andrew",
+    "age": 25,
+    "country": "Japan"
+}
+
+
+user["age"] = 26
+user["role"] = "developer"
+print(user)
+
+
+#question 3
+product = {
+    "name": "Laptop",
+    "price": 1200,
+    "stock": 10
+}
+
+#create a variable
+new_price = 1000
+
+
+
+product["price"] = new_price
+product["brand"] = "MSI"
+
+print(product)
+
+
