3b63a92e44e6ced536abdce0f6c9ec9f7bf45306
Andrew Murphy <legionastartesofficial@gmail.com>
2026-04-25 16:36:25 +0900

    Add files via upload

---
+++ week 9/nested_loops_questions.py
@@ -0,0 +1,58 @@
+#question 1
+
+for row in range(2):
+    for col in range(3):
+        print("*", end=" ")
+    print()
+
+#2 rows 
+#per row has 3 columns
+#new line row 
+
+
+#question 2 
+for row in range(3):
+    for col in range(2):
+        print("#", end=" ")
+    print()
+
+#3 rows 
+# per row has 2 columns 
+# new line row 
+   
+
+#question 3 
+
+for row in range(4):
+    for col in range(1):
+        print("A", end=" ")
+    print()
+
+# 3 rows 
+# 1 column 
+#per line row 
+
+#question 4 
+for row in range(1):
+    for col in range(5):
+        print("B", end=" ")
+    print()
+
+#1 row 
+#4 columns 
+#per line row 
+
+
+#question 5 
+for row in range(2):
+    for col in range(2):
+        print(row, col) #this makes a new line auto maticaly ! 
+
+
+#the column must finish until we loop to the next row !
+#the face that we have two values in print(row, col) makes the structure look weird ! 
+
+0 , 0 
+0 , 1 
+1, 0 
+1, 1 
