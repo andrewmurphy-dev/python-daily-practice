2db1f2684f456ccdff546017b250d240e789777a
Andrew Murphy <legionastartesofficial@gmail.com>
2026-05-02 23:16:34 +0900

    Add add_expenses function for expense tracking
    Implemented add_expenses function to input product details and validate entries.

--- week 10/utils_test.py
+++ week 10/utils_test.py
@@ -1,6 +1,32 @@
 #so looks like we need to rewrite add_epenses function 
 
 
+def add_expenses():
+    product_name = input("enter the name of the product: ").lower().strip()
+
+    if product_name == "":
+        print("error, cannot be blank, please try again!")
+        return 
+
+    product_amount = input(f"enter amount of {product_name}: ")
+    if product_amount == "":
+        print("error, cannot be blank, please try again!")
+        return
+
+    if not product_amount.isdigit():
+            print("error, invalid amount, please try again!")
+            return
+    
+    try:
+        amount = int(product_amount)
+        
+    except ValueError:
+        print("error, invalid amount, please try again!")
+        return
+    
+    expenses[product_name] = amount
+    save_expenses()
+    print("your expenses have been added")
 
 
 
