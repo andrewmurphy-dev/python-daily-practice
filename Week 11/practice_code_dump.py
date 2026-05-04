f240a05adb5a6dedc86d9f8dedfcca23db295f30
Andrew Murphy <legionastartesofficial@gmail.com>
2026-05-04 15:04:45 +0900

    Add files via upload

---
+++ Week 11/practice_code_dump.py
@@ -0,0 +1,16 @@
+import json
+
+expenses = [
+    {"name": "coffee", "amount": 500},
+    {"name": "train", "amount": 220},
+    {"name": "lunch", "amount": 900}
+]
+
+
+
+with open("expenses.json", "w", encoding="utf-8") as file:
+    json.dump(expenses, file, indent=4)
+
+
+
+print("expenses saved to JSON file")
