e5f0c638fb2526113853b1d487a5a7cbad11d151
Andrew Murphy <legionastartesofficial@gmail.com>
2026-05-05 14:26:55 +0900

    Update load.py

--- Week 11/load.py
+++ Week 11/load.py
@@ -10,3 +10,17 @@ from json import JSONDecodeError
 #we do not need a parameter ! because we are not calling the function and implementing data into it !
 #so we need to use try, except for this ducntion because there might be an error loading it from disk to ram !
 
+
+def load_expenses():
+    try:
+        with open("expenses.txt", "r", encoding="utf-8") as file:
+        #so here it gets a little out of the ordinary for patterns!
+
+        return json.load(file)
+
+    except FileNotFoundError:
+        return []
+
+    except JSONDecodeError:     #error if the file is not json file !
+        print("Error: expenses.json exists, but it is not valid JSON.")
+        return None
