7f1f53fd76b9e6a0db3bfb663b5690b95ea0f77a
Andrew Murphy <legionastartesofficial@gmail.com>
2026-05-08 00:01:10 +0900

    Update slciing_questions.py

--- Week 11/slciing_questions.py
+++ Week 11/slciing_questions.py
@@ -21,4 +21,24 @@ def reverse(text):
 #why do we use minus 1?
 #revese starts at 1 not 0
 
+#question 4
+def middle_text(text):
+    return text[2:5:1]
 
+
+#question 5
+def remove_first_two(text):
+    return text[2:len(text):1]
+
+#question 6
+def remove_last_two(text):
+    return text[len(text) - 2: len(text) - 1: -1]
+
+#question 7
+
+def every_second_char(text):
+    return text[2:len(text): 2]
+
+#question 8
+def every_second_reverse(text):
+    return text[len(text): len(text) - 2: -2]
