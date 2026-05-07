e6deeacb70b7f9f3f52b46a09f6f5d7fe77bab15
Andrew Murphy <legionastartesofficial@gmail.com>
2026-05-07 23:59:47 +0900

    Update slciing_questions.py

--- Week 11/slciing_questions.py
+++ Week 11/slciing_questions.py
@@ -3,6 +3,14 @@ def first_three(text):
     return text[:3]
 
 
+#question 2
+
+def last_three(text):
+    return text[len(text) - 3: len(text): 1]
+
+
+result = last_three("Steptrace")
+print(result)
 
 
 
