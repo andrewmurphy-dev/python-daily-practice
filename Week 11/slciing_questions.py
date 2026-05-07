a8fe7ce8c97eefc53fb3d922be545fc0d7d24223
Andrew Murphy <legionastartesofficial@gmail.com>
2026-05-08 00:34:19 +0900

    Add string manipulation functions

--- Week 11/slciing_questions.py
+++ Week 11/slciing_questions.py
@@ -1,1 +1,44 @@
+#question 1
+def first_three(text):
+    return text[:3]
 
+
+#question 2
+
+def last_three(text):
+    return text[len(text) - 3: len(text): 1]
+
+
+result = last_three("Steptrace")
+print(result)
+
+
+
+#question 3
+
+def reverse(text):
+    return text[len(text) - 1: len(text) - 1: -1]
+#why do we use minus 1?
+#revese starts at 1 not 0
+
+#question 4
+def middle_text(text):
+    return text[2:5:1]
+
+
+#question 5
+def remove_first_two(text):
+    return text[2:len(text):1]
+
+#question 6
+def remove_last_two(text):
+    return text[len(text) - 2: len(text) - 1: -1]
+
+#question 7
+
+def every_second_char(text):
+    return text[2:len(text): 2]
+
+#question 8
+def every_second_reverse(text):
+    return text[len(text): len(text) - 2: -2]
