7dc3820f13fd4d94baa70eebb4b8705edb84e5aa
Andrew Murphy <legionastartesofficial@gmail.com>
2026-05-12 20:50:20 +0900

    Update Day_2.py

--- week 12/Day_2.py
+++ week 12/Day_2.py
@@ -1,1 +1,31 @@
+#day 2 focuses on safe number conversion
+#lets do some real world examples
 
+def get_quantity():
+
+    while True:
+        quantity_text = input("Enter quantity: ").strip()
+
+        if quantity_text == "":
+            print("Quantity cannot be empty!")
+            continue
+
+
+        try:
+            quantity = int(quantity_text)
+
+        except ValueError:
+            print("Quantity must be a number!")
+            continue
+
+
+        if quantity < 1:
+            print("invalid quantity!")
+            continue
+
+        elif quantity > 10:
+            print("invalid quanitity")
+            continue
+
+
+        return quantity
